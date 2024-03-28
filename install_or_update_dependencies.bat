eac72e5e044a79c97d0ce9db451e554095e3241b
aramadan0096 <57462754+aramadan0096@users.noreply.github.com>
2024-03-28 05:32:08 +0200

    Update install_or_update_dependencies.bat

--- install_or_update_dependencies.bat
+++ install_or_update_dependencies.bat
@@ -1,2 +1,2 @@
-.\python\python3.11.6\python.exe -m pip install --upgrade diffusers transformers accelerate xformers safetensors rich --no-warn-script-location
+.\python\python3.11.6\python.exe -m pip install --upgrade diffusers transformers accelerate huggingface-hub urllib3 xformers safetensors rich --no-warn-script-location
 pause
